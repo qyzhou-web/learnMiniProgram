548c466c652ff20cf0bad44d61c26217666d286d
qyzhou <qyzhou>
2020-05-10 17:34:27 +0800

    小程序的组件

--- pages/about/about.js
+++ pages/about/about.js
@@ -1,13 +1,84 @@
 // pages/about/about.js
 Page({
-
+  data: {
+    array: [{
+      mode: 'scaleToFill',
+      text: 'scaleToFill：不保持纵横比缩放图片，使图片完全适应'
+    }, {
+      mode: 'aspectFit',
+      text: 'aspectFit：保持纵横比缩放图片，使图片的长边能完全显示出来'
+    }, {
+      mode: 'aspectFill',
+      text: 'aspectFill：保持纵横比缩放图片，只保证图片的短边能完全显示出来'
+    }, {
+      mode: 'top',
+      text: 'top：不缩放图片，只显示图片的顶部区域'
+    }, {
+      mode: 'bottom',
+      text: 'bottom：不缩放图片，只显示图片的底部区域'
+    }, {
+      mode: 'center',
+      text: 'center：不缩放图片，只显示图片的中间区域'
+    }, {
+      mode: 'left',
+      text: 'left：不缩放图片，只显示图片的左边区域'
+    }, {
+      mode: 'right',
+      text: 'right：不缩放图片，只显示图片的右边边区域'
+    }, {
+      mode: 'top left',
+      text: 'top left：不缩放图片，只显示图片的左上边区域'
+    }, {
+      mode: 'top right',
+      text: 'top right：不缩放图片，只显示图片的右上边区域'
+    }, {
+      mode: 'bottom left',
+      text: 'bottom left：不缩放图片，只显示图片的左下边区域'
+    }, {
+      mode: 'bottom right',
+      text: 'bottom right：不缩放图片，只显示图片的右下边区域'
+    }],
+    src: '../../assets/xz.jpg'
+  },
+  imageError: function(e) {
+    console.log('image3发生error事件，携带值为', e.detail.errMsg)
+  },
   /**
    * 页面的初始数据
    */
-  data: {
+  
+  handleContact(even){
+    console.log(even.detail.query)
+    console.log(even.detail.path)
 
   },
+  bindgetuserinfo(e){
+    console.log(e)
 
+  },
+  onScroll(e){
+console.log(e)
+  },
+  onFocus(){
+    console.log("onFucus")
+  },
+  onBlur(){
+    console.log("onBlur")
+  },
+  onInput(){
+    console.log('onInput')
+  },
+  handleChooseAlbum(){
+    wx.chooseImage({
+      success:(res) => {
+        console.log(res)
+        const path = res.tempFilePaths[0]
+        this.setData({
+          imageUrl:path
+        })
+      }
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
